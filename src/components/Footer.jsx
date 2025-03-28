5e36091e1c94294c9537cededb04df622068fcbc
harshbr <harshk2312@gmail.com>
2025-03-28 18:32:10 -0400

    Initial commit of my React project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-gray-800 text-white text-center py-4">
+      <p className="text-xl font-semibold">2024 Harsh Brahmkshatriya</p>
+    </div>
+  );
+}
+
+export default Footer;
